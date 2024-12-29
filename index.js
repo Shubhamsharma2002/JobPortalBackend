98d4e2fb23522163dbaae02597deb6324800f769
shubham sharma <shubhamtech004@gmail.com>
2024-12-29 23:54:38 +0530

    env setup done for this project

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 
+import 'dotenv/config';
 
 import {server} from './app.js';
 
-const port = 5000;
-// process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
 
 server.listen(port, ()=>{
    
