7a3990918f51f166f86ec68099327d7d3ed9d190
shubham sharma <shubhamtech004@gmail.com>
2024-12-30 00:05:15 +0530

    db connection done

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 import 'dotenv/config';
 
 import {server} from './app.js';
+import connectDB from './Src/db/dbConnection.js';
 
 const port = process.env.PORT || 5000;
 
 
 server.listen(port, ()=>{
-   
+   connectDB();
     console.log(`server is fired sucessfully on port number ${port}`);
 })
