1e6eb7ad7a45cb5f03626ab994d729f0cb27d092
shubham sharma <shubhamtech004@gmail.com>
2025-01-16 23:45:27 +0530

    creating the company models

--- Src/Models/companyModel.js
+++ Src/Models/companyModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const companySchema = new mongoose.Schema({
 
 
-    
+    // here some logic
  });
 
 export const Company = mongoose.model("Company", companySchema);
