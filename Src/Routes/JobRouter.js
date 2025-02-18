72e9ebfaadf4123932f03786ef32908625f310e9
shubham sharma <shubhamtech004@gmail.com>
2025-02-18 23:02:36 +0530

    job routes setup

--- Src/Routes/JobRouter.js
+++ Src/Routes/JobRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
 
 import jwtAuth from "../Middleware/jwtAuthMiddleware.js";
+import { getAdminJobs, getAlljob, getJobbyId, postJobs } from "../Controller/JobController.js";
 
 const jobRouter = express.Router();
 
-
+jobRouter.route("/post-Jobs").post(postJobs);
+jobRouter.route("/get-Alljobs").post(getAlljob);
+jobRouter.route("/get-jobById").get(getJobbyId);
+jobRouter.route("/get-jobByadmin").get(getAdminJobs);
 
 jobRouter.route("/check").get((req,res)=>{
     const data = {
