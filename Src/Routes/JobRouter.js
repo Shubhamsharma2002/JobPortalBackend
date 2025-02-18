70e15c870dd5e2252a099be7f8b89121f6b9a690
shubham sharma <shubhamtech004@gmail.com>
2025-02-18 23:05:34 +0530

    code debuging

--- Src/Routes/JobRouter.js
+++ Src/Routes/JobRouter.js
@@ -5,11 +5,11 @@ import { getAdminJobs, getAlljob, getJobbyId, postJobs } from "../Controller/Job
 
 const jobRouter = express.Router();
 // jobs routes
-jobRouter.route("/post-Jobs").post(postJobs);
-jobRouter.route("/get-Alljobs").post(getAlljob);
-jobRouter.route("/get-jobById").get(getJobbyId);
+jobRouter.route("/post-Jobs").post(jwtAuth,postJobs);
+jobRouter.route("/get-Alljobs").get(jwtAuth,getAlljob);
+jobRouter.route("/get-jobById").get(jwtAuth,getJobbyId);
 // geting job on only admin dashboard
-jobRouter.route("/get-jobByadmin").get(getAdminJobs);
+jobRouter.route("/get-jobByadmin").get(jwtAuth,getAdminJobs);
 // test routes
 jobRouter.route("/check").get((req,res)=>{
     const data = {
