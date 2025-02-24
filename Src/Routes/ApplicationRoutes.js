138b1cf6dfe71b32ab8de0e88d7efff8cf80c56d
shubham sharma <shubhamtech004@gmail.com>
2025-02-24 23:03:55 +0530

    code debuging

--- Src/Routes/ApplicationRoutes.js
+++ Src/Routes/ApplicationRoutes.js
@@ -6,11 +6,11 @@ import jwtAuth from "../Middleware/jwtAuthMiddleware.js";
 const applicationRouter = express.Router();
 
 
-applicationRouter.route("/check").get((req,res)=>{
-    const data = {
-        message: "Hello, this is a JSON response by application routes"
-    };
-    res.json(data); // Sends a JSON response
-})
+// applicationRouter.route("/check").get((req,res)=>{
+//     const data = {
+//         message: "Hello, this is a JSON response by application routes"
+//     };
+//     res.json(data); // Sends a JSON response
+// })
 
 export default applicationRouter;
