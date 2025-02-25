6509b029db12312d8366137832fd50c299821361
shubham sharma <shubhamtech004@gmail.com>
2025-02-25 22:16:22 +0530

    setup and testing done for the resuesbale class api error and response

--- Src/utils/Apiresponse.js
+++ Src/utils/Apiresponse.js
@@ -1,16 +1,3 @@
-// class ApiResponse{
-
-//     constructor(statusCode,data,message = "success"){
-//         this.statusCode = statusCode
-//         this.dta = data
-//         this.message = message
-//         this.success = statusCode < 400
-//     }
-// }
-
-
-
-
 class ApiResponse {
     constructor(statusCode, data, message = "success") {
         this.statusCode = statusCode;
