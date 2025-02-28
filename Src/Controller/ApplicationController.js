46ac9eeafdcde9fa1b964b04a584bd9955324158
shubham sharma <shubhamtech004@gmail.com>
2025-02-28 21:54:12 +0530

    testing application routes and debuging the codes

--- Src/Controller/ApplicationController.js
+++ Src/Controller/ApplicationController.js
@@ -88,7 +88,7 @@ export const getApplication = async(req,res)=>{
          const jobId = req.params.id;
          const job = await Job.findById(jobId)
          .populate({
-            path:'Application',
+            path:'applications',
             options:{sort:{createdAt:-1}},
             populate:{
                 path:'application'
